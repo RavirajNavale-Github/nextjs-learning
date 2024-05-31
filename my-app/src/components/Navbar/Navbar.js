df9d1890ca0d3c8ed821ce856aef4e9f132dd4d6
RavirajNavale-Github <165894463+RavirajNavale-Github@users.noreply.github.com>
2024-05-31 13:09:19 +0530

    Removed Link andd used useRouter

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -1,35 +1,58 @@
-import Link from "next/link";
+'use client'
+// import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+
+  const router = useRouter();
+
   return (
     <div>
       <nav className="bg-gray-800 ">
         <div className="flex space-x-4 p-2">
-          <Link
+          {/* <Link
             href="/"
             className="text-white hover:bg-gray-700 rounded-md px-10 py-2 text-sm font-medium"
             aria-current="page"
           >
             Home
-          </Link>
-          <Link
+          </Link> */}
+          <button
+            className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-10 py-2 text-sm font-medium"
+            onClick={() => router.push('/')}
+          >
+            Home
+          </button>
+          {/* <Link
             href="/aboutus"
             className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-10 py-2 text-sm font-medium"
           >
             AboutUs
-          </Link>
-          <Link
+          </Link> */}
+          <button
+            className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-10 py-2 text-sm font-medium"
+            onClick={() => router.push('/aboutus')}
+          >
+            AboutUs
+          </button>
+          {/* <Link
             href="/contactus"
             className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-10 py-2 text-sm font-medium"
           >
             ContactUs
-          </Link>
-          <Link
-            href="/addblog"
+          </Link> */}
+          <button
             className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-10 py-2 text-sm font-medium"
+            onClick={() => router.push('/contactus')}
+          >
+            ContactUs
+          </button>
+          <button
+            className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-10 py-2 text-sm font-medium"
+            onClick={() => router.push('/addblog')}
           >
             Add Blog
-          </Link>
+          </button>
         </div>
       </nav>
     </div>
